76d3707c4efff388a8f3e9f3fa87f68a3ffb37de
Strifewind <loudmouthbrian@gmail.com>
2022-05-15 18:36:55 -0700

    work done on example 7.2 to be updated.

--- python_edu/ex_7_2/ex_7_2.py
+++ python_edu/ex_7_2/ex_7_2.py
@@ -1,8 +1,22 @@
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
+#a loop to find the instances of X-DSPAM-Confidence
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:"):
         continue
-    print(line)
+    print(line.rstrip())
+#a set of variables to pull the floats out of X-DSPAM-Confidence
+    col = line.find(":")
+    fl = line[col+2:]
+    print(fl)
+
+#a recursive function to sum the numbers from fl
+def sum_numbers(numbers):
+    if len(numbers) == 0:
+        return 0
+    return numbers[0] + sum_numbers(numbers[1:])
+
+sum_numbers(fl)
+
 print("Done")
